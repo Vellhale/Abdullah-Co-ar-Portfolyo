34d13b366a160569c4ca417dd83ecb2afc788a53
Vellhale <abdullahgamingtr93@gmail.com>
2026-02-03 00:53:52 +0300

    Add files via upload

---
+++ DERS_NOT_HESAPLAMA/DERS_NOT_HESAPLAMA/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace DERS_NOT_HESAPLAMA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.verileriOkuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dosyaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kaydetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.çıkışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(84, 39);
+            this.textBox1.MaxLength = 20;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(242, 29);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(84, 75);
+            this.textBox2.MaxLength = 20;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(242, 29);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "GENEL MATEMATİK",
+            "GÖRSEL PROGRAMLAMA",
+            "İKTİSADA GİRİŞ",
+            "VERİ YAPILARI II",
+            "WEB PROGRAMLAMA"});
+            this.comboBox1.Location = new System.Drawing.Point(84, 105);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(242, 32);
+            this.comboBox1.Sorted = true;
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(84, 137);
+            this.textBox3.MaxLength = 3;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(61, 29);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(176, 137);
+            this.textBox4.MaxLength = 3;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(61, 29);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(265, 137);
+            this.textBox5.MaxLength = 3;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(61, 29);
+            this.textBox5.TabIndex = 5;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            this.textBox5.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox5_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(84, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(242, 75);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "SİSTEME KAYDET";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 17;
+            this.listBox1.Location = new System.Drawing.Point(345, 45);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(388, 191);
+            this.listBox1.TabIndex = 7;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.verileriOkuToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(187, 58);
+            // 
+            // verileriOkuToolStripMenuItem
+            // 
+            this.verileriOkuToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.verileriOkuToolStripMenuItem.Image = global::DERS_NOT_HESAPLAMA.Properties.Resources.Custom_Icon_Design_Pretty_Office_9_File_complete_ico_48x48;
+            this.verileriOkuToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.verileriOkuToolStripMenuItem.Name = "verileriOkuToolStripMenuItem";
+            this.verileriOkuToolStripMenuItem.Size = new System.Drawing.Size(186, 54);
+            this.verileriOkuToolStripMenuItem.Text = "Verileri Oku";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 24);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Öğr. Adı :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 24);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Soyadı :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 24);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Dersi :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(2, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 24);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "V / F / Ö :";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Yellow;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dosyaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(738, 28);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dosyaToolStripMenuItem
+            // 
+            this.dosyaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kaydetToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.çıkışToolStripMenuItem});
+            this.dosyaToolStripMenuItem.Name = "dosyaToolStripMenuItem";
+            this.dosyaToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
+            this.dosyaToolStripMenuItem.Text = "&Dosya";
+            // 
+            // kaydetToolStripMenuItem
+            // 
+            this.kaydetToolStripMenuItem.Image = global::DERS_NOT_HESAPLAMA.Properties.Resources.Oxygen_Icons_org_Oxygen_Actions_document_save_as_ico_48x48;
+            this.kaydetToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.kaydetToolStripMenuItem.Name = "kaydetToolStripMenuItem";
+            this.kaydetToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.kaydetToolStripMenuItem.Size = new System.Drawing.Size(206, 54);
+            this.kaydetToolStripMenuItem.Text = "Kaydet";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(203, 6);
+            // 
+            // çıkışToolStripMenuItem
+            // 
+            this.çıkışToolStripMenuItem.Image = global::DERS_NOT_HESAPLAMA.Properties.Resources.Jommans_Ironman_Style_File_Music_ico_48x48;
+            this.çıkışToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
+            this.çıkışToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(206, 54);
+            this.çıkışToolStripMenuItem.Text = "Çıkış";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(738, 256);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dosyaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kaydetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem çıkışToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem verileriOkuToolStripMenuItem;
+    }
+}
+
---
+++ DERS_NOT_HESAPLAMA/DERS_NOT_HESAPLAMA/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DERS_NOT_HESAPLAMA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Text = "Ders Not Hesaplama " + DateTime.Now;
+
+
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >= 48 && e.KeyChar <= 57 || e.KeyChar ==8)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+            if (e.KeyChar == 13) { textBox4.Focus(); }
+
+
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >= 48 && e.KeyChar <= 57 || e.KeyChar == 8)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+            if (e.KeyChar == 13) { textBox5.Focus(); }
+        }
+
+        private void textBox5_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >= 48 && e.KeyChar <= 57 || e.KeyChar == 8)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+            if (e.KeyChar == 13) { button1.Focus(); }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >=65 && e.KeyChar <=90 ||
+               e.KeyChar >=97 && e.KeyChar <=122  ||
+            e.KeyChar == 8)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+            if (e.KeyChar == 13) { textBox2.Focus(); }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >= 65 && e.KeyChar <= 90 ||
+               e.KeyChar >= 97 && e.KeyChar <= 122 ||
+            e.KeyChar == 8)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+            if (e.KeyChar == 13) { textBox3.Focus(); }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            int kontrol;
+            if (int.TryParse(textBox3.Text , out kontrol ))
+            {
+                if(kontrol>100)
+                {
+                    MessageBox.Show("100 den büyük sayı yazamazsın!!!");
+                    textBox3.Clear();
+                }
+
+            }
+
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            int kontrol;
+            if (int.TryParse(textBox4.Text, out kontrol))
+            {
+                if (kontrol > 100)
+                {
+                    MessageBox.Show("100 den büyük sayı yazamazsın!!!");
+                    textBox4.Clear();
+                }
+
+            }
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            int kontrol;
+            if (int.TryParse(textBox5.Text, out kontrol))
+            {
+                if (kontrol > 100)
+                {
+                    MessageBox.Show("100 den büyük sayı yazamazsın!!!");
+                    textBox5.Clear();
+                }
+
+            }
+        }
+        DialogResult mavi;
+        Int32 vize, final, odev; double ortalama;
+        string durumu = ""; string harf_notu = "";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" ||
+                textBox2.Text =="")
+            {
+                MessageBox.Show("ÖĞRENCİNİN ADI - SOYADI BOŞ OLAMAZ");
+                return;
+            }
+            if (comboBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("SINAV OLDUĞU DERSİ SEÇİNİZ");
+                return;
+            }
+            if (textBox3.Text == "" ||
+              textBox4.Text == "" || textBox5.Text =="")
+            {
+                MessageBox.Show("ÖĞRENCİNİN VİZE - FİNAL - ÖDEV NOTLARI BOŞ OLAMAZ");
+                return;
+            }
+
+            //İŞLEM KAYIT
+            mavi =MessageBox.Show (textBox1.Text +" "+
+                textBox2.Text +" isimli öğrenci kaydı yapılsın mı?",
+                "ÖĞRENCİ KAYIT",MessageBoxButtons.YesNo ,
+                MessageBoxIcon.Question );
+            if (mavi == DialogResult.Yes)
+            {
+                vize = Convert.ToInt32(textBox3.Text);
+                final  = Convert.ToInt32(textBox4.Text);
+                odev  = Convert.ToInt32(textBox5.Text);
+                ortalama = vize * 0.30 + final * 0.60 + odev * 0.10;
+                if (ortalama < 60)
+                {
+                    durumu = "DERSTEN KALDI";
+                }
+                else 
+                {
+                durumu ="DERSTEN GEÇTİ";
+                }
+                //0-34ff 35-40fd 41-50dd 51-59dc 60-70cc 7180bc 8190ba 91100aa
+                if (ortalama >= 0 && ortalama <= 34) {harf_notu="FF"; }
+                if (ortalama >= 35 && ortalama <= 40) { harf_notu = "FD"; }
+                if (ortalama >= 41 && ortalama <= 50) { harf_notu = "DD"; }
+                if (ortalama >= 51 && ortalama <= 59) { harf_notu = "DC"; }
+                if (ortalama >= 60 && ortalama <= 70) { harf_notu = "CC"; }
+                if (ortalama >= 71 && ortalama <= 80) { harf_notu = "CB"; }
+                if (ortalama >= 81 && ortalama <= 90) { harf_notu = "BB"; }
+                if (ortalama >= 91 && ortalama <= 100) { harf_notu = "AA"; }
+                listBox1.Items.Add(textBox1.Text+" "+textBox2.Text+" "+comboBox1.SelectedItem+" "+ ortalama+ " "+durumu+" "+harf_notu);
+                StreamWriter kaydet = new StreamWriter("kayit.txt", true, Encoding.Default);
+                kaydet.WriteLine(textBox1.Text + " " + textBox2.Text + " " + comboBox1.SelectedItem + " " + ortalama + " " + durumu + " " + harf_notu);
+                kaydet.Close();
+                MessageBox.Show("TÜM BİLGİLER KAYDEDİLDİ");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
